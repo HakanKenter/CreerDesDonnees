6513c1b2894ac2dd6bba875cb51f9b0f0312cdc9
hakan <hakan.kt@outlook.fr>
2022-07-14 22:48:12 +0200

    first commit

--- CreerDesDonnees/ViewController.swift
+++ CreerDesDonnees/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var numberTF: UITextField!
+    @IBOutlet weak var secureTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(hideKeyboard)))
         // Do any additional setup after loading the view.
     }
+    
+    @objc func hideKeyboard() {
+        // On va forcer la vue a arreter d'editer
+        view.endEditing(true)
+    }
 
-
+    @IBAction func nameTFPressed(_ sender: UITextField) {
+        // Editing End = clavier se ferme
+        // Cette fonction est appelé quand le clavier se ferme
+        print(nameTF.text)
+    }
+    
+    
 }
 
